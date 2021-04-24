98e8610e515f3b310f142b461e424e7c2ca1bb49
Prashant Dhage <dhage.prashant@gmail.com>
2021-04-24 13:04:39 +0530

    Update menu-item with navigation capability with Router HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
 /**
  * {title} ---here destructuring is used instead of passing props & use it as props.title
  * 
  */
-const MenuItem = ({title,imageUrl,size})=>(
+const MenuItem = ({title,imageUrl,size,history,linkUrl,match})=>(
    
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
     <div className='background-image' style={{backgroundImage:`url(${imageUrl})`}} />
       <div className='content'>
         <div className='title'>{title.toUpperCase()}</div>
@@ -16,4 +17,4 @@ const MenuItem = ({title,imageUrl,size})=>(
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
