455921cd1005355049dd34bd2e0f55bc440bcfab
Prashant Dhage <dhage.prashant@gmail.com>
2021-04-25 23:03:10 +0530

    Added shop page, preview-collection & collection-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -9,7 +9,7 @@ import {withRouter} from 'react-router-dom';
 const MenuItem = ({title,imageUrl,size,history,linkUrl,match})=>(
    
     <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
-    <div className='background-image' style={{backgroundImage:`url(${imageUrl})`}} />
+    <div className='background-image' style={{backgroundImage:`url(${imageUrl})`}}  />
       <div className='content'>
         <div className='title'>{title.toUpperCase()}</div>
         <span className='subtitle'>SHOP NOW</span>
